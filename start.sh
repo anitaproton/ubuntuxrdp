373da5018675f849bc6a3c0c6f6bff173f055eb4
anitaproton <anitasingh1990@protonmail.com>
2026-07-20 15:24:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-
 systemctl enable xrdp
 
 systemctl set-default graphical.target
 
 service xrdp start
 
+mkdir -p /tmp/.X11-unix
+chmod 1777 /tmp/.X11-unix
+tail -f /var/log/xrdp-sesman.log
 
